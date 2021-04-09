c1f254d53c27410f6ea45f5a8781ac1a0d70c578
Lish <tlbgzjc@163.com>
2021-04-09 15:48:22 -0700

    use rotate

---
+++ zzh/main/static/main/circle.js
@@ -0,0 +1,44 @@
+$(function() {
+  console.log($(window).width());
+
+  var left = ($(window).width() - 700) / 2;
+  var top = ($(window).height() - 700) / 2;
+  $(".circle").css({
+      "top": top,
+      "left": left,
+    });
+
+
+  var rotation = 0, 
+    scrollLoc = $(document).scrollTop();
+  $(window).scroll(function() {
+    var newLoc = $(document).scrollTop();
+    var diff = scrollLoc - newLoc;
+    rotation += diff, scrollLoc = newLoc;
+    var rotationStr = "rotate(" + rotation + "deg)";
+    $(".circle").css({
+      "-webkit-transform": rotationStr,
+      "-moz-transform": rotationStr,
+      "transform": rotationStr
+    });
+  });
+
+  // var elems = document.querySelectorAll('div.clock div');
+  //   var increase = Math.PI * 2 / elems.length;
+  //   var x = 0, y = 0, angle = 0, elem;
+
+  //   for (var i = 0; i < elems.length; i++) {
+  //     var radius = 120;
+  //     if (i % 2)
+  //       console.log("x=" + x);
+  //       console.log("y=" + y);
+  //       console.log("angle=" + angle);
+  //       radius = 115 
+  //       elem = elems[i];
+  //       x = radius * Math.cos(angle) + 200;
+  //       y = radius * Math.sin(angle) + 200;
+  //       elem.style.left = (x) + 'px';
+  //       elem.style.top = (y) + 'px';
+  //       angle += increase;
+  //   }
+})
